298d6c2097dd9b66e1f2ec864ccd37d617ecd67a
Pedro Henrique <pedrohenriquefp_2013@hotmail.com>
2024-06-17 12:10:10 -0300

    Adding pattern repository

--- app/src/main/java/com/example/archtraining/PatternRepository.kt
+++ app/src/main/java/com/example/archtraining/PatternRepository.kt
@@ -1,4 +1,7 @@
 package com.example.archtraining
 
-class PatternRepository {
+class PatternRepository(private val dataSource: DataSource) {
+    fun getPatternByName(name: String): PatternDTO? {
+        return dataSource.getPatterns().find { it.name.equals(name, ignoreCase = true) }
+    }
 }
