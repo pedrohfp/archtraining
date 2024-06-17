d256a49677124bfb55c6b842bb2778c3c963ccfe
Pedro Henrique <pedrohenriquefp_2013@hotmail.com>
2024-06-17 13:11:40 -0300

    Adding viewBinding on pattern activity

--- app/src/main/java/com/example/archtraining/PatternActivity.kt
+++ app/src/main/java/com/example/archtraining/PatternActivity.kt
@@ -2,12 +2,16 @@ package com.example.archtraining
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.archtraining.databinding.ActivityPatternBinding
 
 class PatternActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityPatternBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_pattern)
+        binding = ActivityPatternBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
